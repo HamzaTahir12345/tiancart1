5f6326a92c3c14c404a8c9d06c5d35021ba6006f
Hamza <tahirhamza41@gmail.com>
2021-11-25 23:37:41 +0500

    forgetpassword

--- ecommerce/routes/user.js
+++ ecommerce/routes/user.js
@@ -9,6 +9,7 @@ const {
   update,
   purchaseHistory,
   forgetPasswordLink,
+  forgetPassword,
 } = require("../controllers/user");
 
 router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
@@ -20,7 +21,8 @@ router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
 router.get("/user/:userId", requireSignin, isAuth, read);
 router.get("/orders/by/user/:userId", requireSignin, isAuth, purchaseHistory);
 router.put("/user/:userId", requireSignin, isAuth, update);
-router.post("forget-password-link", forgetPasswordLink);
+router.post("/forget-password-link", forgetPasswordLink);
+router.post("/forget-password", forgetPassword);
 router.param("userId", userById);
 
 module.exports = router;
